4714c144031a63319e8752824bf95155b4ca3956
Tamas Kapitany <kapitanytms@gmail.com>
2018-05-10 23:49:47 +0200

    Updating dependencies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {AuthGuardService} from './auth/auth-guard.service';
+import {AuthModule} from './auth/auth.module';
+import {TweetModule} from './tweets/tweet.module';
+import {SearchModule} from './search/search.module';
 
 const appRoutes: Routes = [
-    {path: 'welcome', loadChildren: './auth/auth.module#AuthModule'},
-    {path: 'home', loadChildren: './tweets/tweet.module#TweetModule', canActivate: [AuthGuardService]},
-    {path: 'search', loadChildren: './search/search.module#SearchModule', canActivate: [AuthGuardService] }
+    {path: 'welcome', loadChildren: () => AuthModule},
+    {path: 'home', loadChildren: () => TweetModule, canActivate: [AuthGuardService]},
+    {path: 'search', loadChildren: () => SearchModule, canActivate: [AuthGuardService] }
 ];
 
 @NgModule({
